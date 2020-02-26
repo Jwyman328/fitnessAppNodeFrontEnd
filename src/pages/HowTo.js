66bd48962c1ee5d77ef8ee3e934fd6ffd6227507
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-02-26 12:50:49 -0500

    Add css to rules and howTo page

--- src/pages/HowTo.js
+++ src/pages/HowTo.js
@@ -1,40 +1,75 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HowTo.css";
+import "./Rules.css";
+
 function HowTo(props) {
-    return (
-        <div>
-            How To
-        
-        <h2> How to: Fitness Challenge</h2>
-        <br></br>
+  return (
+    <div className="rulePageContainer">
+      <div className="containerRules">
+        <h1 className="mainTitle">How to: Fitness Challenge</h1>
 
-        <h3> Step 1: Create a User: <Link to='/signup' >Signup  </Link>{/*signup */}  </h3>
-       
-        <h3> Step 2: Compete against expectations: <Link to='/GoalPage' >Create Goal  </Link>  </h3>
-        <ul>
-            <li> Create daily point goals to see if you can beat your expectations </li>
-        </ul>
-        
-        <h3> Step 3: Start Tracking: <Link to='/inputPoints' >Signup  </Link></h3>
-        <ul>
-        <li> Input your health data to generate points, including sleeping hours, workout time, steps, water and more </li>
-        <br></br>
-        </ul>
-        <h3> Step 4: View Results:{/*view results */}</h3>
-        <ul>
-        <li> View your health data graphs, tables and statistics  </li>
-        <br></br>
-        </ul>
-
-        
-        <h3> Step 5: Create and Compete in Challenges: <Link to='/Challenges' >Create Challenge </Link>  </h3>
-        <ul>
-        <li> Create a challenge for any fitness category and Invite your friends to compete</li>
-        <li> Accept challenge invitations from others: <Link to='/PendingChallenges'>Pending challenges</Link></li>
-        <li> View current challenge data and leader board standings for each challenge:<Link to='/CurrentFutureChallenges'>Current challenges</Link></li>
-        </ul>
+        <div className="subContainer">
+          <h2 className="subTitle">
+            Step 1: Create a User: <Link to="/signup">Signup </Link>
+          </h2>
         </div>
-    );
+
+        <div className="subContainer">
+          <h2 className="subTitle">
+            Step 2: Compete against expectations:
+            <Link to="/GoalPage">Create Goal</Link>
+          </h2>
+          <ul>
+            <li className="subDiscrip">
+              Create daily point goals and see if you can beat your expectations
+            </li>
+          </ul>
+        </div>
+
+        <div className="subContainer">
+          <h2 className="subTitle">
+            Step 3: Start Tracking: <Link to="/inputPoints">Input points</Link>
+          </h2>
+          <ul>
+            <li className="subDiscrip">
+              Input health data to generate points, including sleep, workout time, steps, and more
+            </li>
+          </ul>
+        </div>
+
+        <div className="subContainer">
+          <h2 className="subTitle">Step 4: View Results:{/*view results */}</h2>
+          <ul>
+            <li className="subDiscrip">
+              View your health data graphs, tables and statistics
+            </li>
+          </ul>
+        </div>
+        <div className="subContainer">
+          <h2 className="subTitle">
+            Step 5: Create/Compete in Challenges:
+            <Link to="/Challenges">Create Challenge</Link>
+          </h2>
+          <ul>
+            <li className="subDiscrip">
+              Create a challenge and Invite your
+              friends to compete
+            </li>
+            <li className="subDiscrip">
+              Accept challenge invitations from others:
+              <Link to="/PendingChallenges">Pending challenges</Link>
+            </li>
+            <li className="subDiscrip">
+              View current challenge data and leader board standings for each
+              challenge:
+              <Link to="/CurrentFutureChallenges">Current challenges</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HowTo;
--- src/pages/Rules.js
+++ src/pages/Rules.js
@@ -1,31 +1,34 @@
 import React from "react";
+import "./Rules.css";
 
 function Rules(props) {
   return (
-    <div>
-      Rules
-      <div >
-        <div >
-          <div >
-            <div >
-              <h2>Scoring System</h2>
-            </div>
-            <ul >
-              <li >
-                Clean Eating Points: Eat Clean the entire day, 10 points
-              </li>
-              <li >
-                Sleep Points: Hours of Sleep * 3.3
-              </li>
-              <li >
-                Workout Points: Workout intensity * (Workout minutes * .2)
-              </li>
-              <li >
-                Water Points: 100 oz of water, 10 points
-              </li>
-              <li >Step Points: Steps * .001</li>
-            </ul>
-          </div>
+    <div className="rulePageContainer">
+      <div className="containerRules">
+        <h1>Scoring System</h1>
+
+        <div className='rulesRow'>
+          <h2 className='ruleTitle'>Clean Eating Points:</h2>
+          <h4 className='ruleDiscrip'>Eat Clean the entire day, 10 points</h4>
+        </div>
+
+        <div className='rulesRow'>
+          <h2 className='ruleTitle'>Sleep Points:</h2>
+          <h4 className='ruleDiscrip'>Hours of Sleep * 3.3</h4>
+        </div>
+
+        <div className='rulesRow'>
+          <h2 className='ruleTitle'>Workout Points:</h2>
+          <h4 className='ruleDiscrip'>Workout intensity * (Workout minutes * .2)</h4>
+        </div>
+
+        <div className='rulesRow'>
+          <h2 className='ruleTitle'>Water Points:</h2>
+          <h4 className='ruleDiscrip'>100 oz of water, 10 points</h4>
+        </div>
+        <div className='rulesRow'>
+          <h2 className='ruleTitle'>Step Points:</h2>
+          <h4 className='ruleDiscrip'>Steps * .001</h4>
         </div>
       </div>
     </div>
