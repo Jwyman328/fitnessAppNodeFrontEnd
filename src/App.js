b05ccca0ad1da2280e774c259e07739c8cb2571b
Joseph Wyman <jwyman@Josephs-MBP.home>
2020-03-06 11:10:07 -0500

    Add lazy loading and suspense

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import React, { useReducer, useContext, useEffect } from "react";
+import React, { useReducer, useContext, useEffect, Suspense } from "react";
 import "./App.css";
+import "./pages/form.css"
+import "./pages/Rules.css"
 
-//pages
-import SignUpPage from "./pages/auth/signUp";
-import HomePage from "./pages/home";
-import LoginPage from "./pages/auth/login";
-import InputPointsPage from "./pages/activity/inputPointsPage";
-import GoalPage from "./pages/goals/GoalsPage";
-import PastGoals from "./pages/goals/PastGoals";
-import GoalsGraphs from "./pages/graphs/GoalsGraph";
-import CurrentFutureGoals from "./pages/goals/currentFutureGoals";
-import ChallengePage from "./pages/challenges/ChallengePage";
-import ViewResults from "./pages/ViewResults";
-import Rules from "./pages/Rules";
-import HowTo from "./pages/HowTo";
-import PendingChallenges from "./pages/challenges/PendingChallenges";
-import PastChallenges from "./pages/challenges/PastChallenges";
-import CurrentFutureChallenges from "./pages/challenges/CurrentFutureChallenges";
 
-import IndividualDailyPointGraph from "./pages/graphs/IndividualDailyPointGraph";
-import UpdateActivityInput from "./pages/activity/UpdateActivityInput";
 //Routing
 import {
   BrowserRouter as Router,
@@ -38,6 +22,25 @@ import ChallengeNavBar from "./components/navBars/challengeNavBar";
 // global state
 import { store } from "./store/globalStore";
 import { getGlobalState, dispatchInputChange } from "./utils/helperFunctions";
+//pages
+const SignUpPage = React.lazy(() => import("./pages/auth/signUp"));
+const HomePage = React.lazy(() => import("./pages/home"));
+const LoginPage = React.lazy(() => import("./pages/auth/login"));
+const InputPointsPage = React.lazy(() => import("./pages/activity/inputPointsPage"));
+const GoalPage = React.lazy(() => import("./pages/goals/GoalsPage"));
+const PastGoals = React.lazy(() => import("./pages/goals/PastGoals"));
+const GoalsGraphs = React.lazy(() => import("./pages/graphs/GoalsGraph"));
+const CurrentFutureGoals = React.lazy(() => import("./pages/goals/currentFutureGoals"));
+const ChallengePage = React.lazy(() => import("./pages/challenges/ChallengePage"));
+const ViewResults = React.lazy(() => import("./pages/ViewResults"));
+const Rules = React.lazy(() => import("./pages/Rules"));
+const HowTo = React.lazy(() => import("./pages/HowTo"));
+const PendingChallenges = React.lazy(() => import("./pages/challenges/PendingChallenges"));
+const PastChallenges = React.lazy(() => import("./pages/challenges/PastChallenges"));
+const CurrentFutureChallenges = React.lazy(() => import("./pages/challenges/CurrentFutureChallenges"));
+
+const IndividualDailyPointGraph =  React.lazy(() => import("./pages/graphs/IndividualDailyPointGraph"));
+const UpdateActivityInput =  React.lazy(() => import("./pages/activity/UpdateActivityInput"));
 
 /**
  * Router component to handle all application routing.
@@ -54,129 +57,153 @@ function App(props) {
     <div className="App">
       <Router>
         {isLoggedIn ? null : (
+           <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             <Route path="/">
               <AuthNavBar />
             </Route>
           </Switch>
+          </Suspense>
         )}
-       
+
         {isLoggedIn ? (
-          <Switch>
+          <Suspense fallback={<div>Loading...</div>}><Switch>
             <Route path="/">
               <LoggedInNavBar />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
 
-        <Switch>
-          <Route exact path="/signup">
-            <SignUpPage />
-          </Route>
-        </Switch>
-        <Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/signup">
+              <SignUpPage />
+            </Route>
+          </Switch>
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}> <Switch>
           <Route exact path="/home">
             <HomePage />
           </Route>
         </Switch>
-        <Switch>
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>  <Switch>
           <Route exact path="/login">
             <LoginPage />
           </Route>
         </Switch>
+        </Suspense>
         {isLoggedIn ? (
-          <Switch>
+         <Suspense fallback={<div>Loading...</div>}> <Switch>
             <Route exact path="/inputPoints">
               <InputPointsPage />
             </Route>
-          </Switch>
+          </Switch></Suspense>
         ) : null}
         {isLoggedIn ? (
-          <Switch>
+       <Suspense fallback={<div>Loading...</div>}>   <Switch>
             <Route exact path="/GoalPage">
               <GoalPage />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
-          <Switch>
+        <Suspense fallback={<div>Loading...</div>}>  <Switch>
             <Route exact path="/GoalsGraph">
               <GoalsGraphs />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
-          <Switch>
+        <Suspense fallback={<div>Loading...</div>}>  <Switch>
             <Route exact path="/CurrentFutureGoals">
               <CurrentFutureGoals />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
-          <Switch>
+         <Suspense fallback={<div>Loading...</div>}> <Switch>
             <Route exact path="/PastGoals">
               <PastGoals />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
-          <Switch>
+         <Suspense fallback={<div>Loading...</div>}> <Switch>
             <Route exact path="/Challenges">
               <ChallengePage />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
-          <Switch>
+         <Suspense fallback={<div>Loading...</div>}> <Switch>
             <Route exact path="/ViewResults">
               <ViewResults />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
-        <Switch>
+       <Suspense fallback={<div>Loading...</div>}> <Switch>
           <Route exact path="/Rules">
             <Rules />
           </Route>
         </Switch>
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/HowTo">
             <HowTo />
           </Route>
         </Switch>
+        </Suspense>
         {isLoggedIn ? (
-          <Switch>
+        <Suspense fallback={<div>Loading...</div>}>  <Switch>
             <Route exact path="/PendingChallenges">
               <PendingChallenges />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
+          <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             <Route exact path="/PastChallenges">
               <PastChallenges />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
-          <Switch>
+        <Suspense fallback={<div>Loading...</div>}>  <Switch>
             <Route exact path="/CurrentFutureChallenges">
               <CurrentFutureChallenges />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
+          <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             <Route exact path="/IndividualDailyPointGraph">
               <IndividualDailyPointGraph />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
         {isLoggedIn ? (
+          <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             <Route exact path="/IndividualActivityPointUpdate">
               <UpdateActivityInput />
             </Route>
           </Switch>
+          </Suspense>
         ) : null}
       </Router>
     </div>
