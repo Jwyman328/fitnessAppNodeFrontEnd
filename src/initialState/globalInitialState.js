a7064ff1f552b7db4b7e0a4eb4cc8f0d38e1e163
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-03-11 12:06:55 -0400

    global state logs

--- src/initialState/globalInitialState.js
+++ src/initialState/globalInitialState.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 /**
  * Initial global state.
+ * 
+ * Check for  token in local storage, if it exists then user is logged in.
  */
 const localToken = JSON.parse(localStorage.getItem('token'))
 const initialState = localToken?{token:localToken,isLoggedIn:true} : {token:'',isLoggedIn:false} 
