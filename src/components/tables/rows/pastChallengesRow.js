702682a627c15c8a386750767af08001bcd4fd2c
Joseph Wyman <jwyman@Josephs-MBP.home>
2020-03-12 13:16:40 -0400

    Installed prettier

--- src/components/tables/rows/pastChallengesRow.js
+++ src/components/tables/rows/pastChallengesRow.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 /**
  * Create a display card for a challenge invitation
  * @param {Object} challengeInvitationData -- challenge invitation data
  */
-const PastChallengesRow =(challengeInvitationData) => {
-
-    return(
-        <tr  key={`${challengeInvitationData._id}`} className='rowContainer' >
-            <td className='rowItem' data-testid='title'>{challengeInvitationData.title}</td>
-            <td className='rowItem' data-testid='challengeType'>{challengeInvitationData.challengeType}</td>
-            <td className='rowItem' data-testid='startDate'>{challengeInvitationData.startDate}</td>
-            <td className='rowItem' data-testid='endDate'>{challengeInvitationData.endDate}</td>
-        </tr>
-    )
-}
+const PastChallengesRow = challengeInvitationData => {
+  return (
+    <tr key={`${challengeInvitationData._id}`} className="rowContainer">
+      <td className="rowItem" data-testid="title">
+        {challengeInvitationData.title}
+      </td>
+      <td className="rowItem" data-testid="challengeType">
+        {challengeInvitationData.challengeType}
+      </td>
+      <td className="rowItem" data-testid="startDate">
+        {challengeInvitationData.startDate}
+      </td>
+      <td className="rowItem" data-testid="endDate">
+        {challengeInvitationData.endDate}
+      </td>
+    </tr>
+  );
+};
 
 export default PastChallengesRow;
