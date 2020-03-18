83522182d2f2b00bbe9f4851a05607708c7cf898
Joseph Wyman <jwymandev@gmail.com>
2020-03-18 12:11:56 -0400

    Left align text

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -58,7 +58,7 @@ function LoginPage(props) {
     <div className="rulePageContainer">
       <div className="containerRules">
         <h1>Login</h1>
-        <div>
+        <div style={{ textAlign: "left" }}>
           <h3>Log in as a guest</h3>
           <h4>email: test_user@test.com</h4>
           <h4>password: test123</h4>
