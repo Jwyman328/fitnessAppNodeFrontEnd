f49a7f78f4b7dc12a1cb1203218c6a2b670907f8
Joseph Wyman <jwymandev@gmail.com>
2020-04-18 11:49:35 -0400

    Add custom global state hook to current future goals page

--- src/pages/goals/currentFutureGoals.js
+++ src/pages/goals/currentFutureGoals.js
@@ -1,26 +1,29 @@
 import React, { useEffect, useReducer, useContext } from "react";
 import { store } from "../../store/globalStore";
-import { getGlobalState, dispatchInputChange } from "../../utils/helperFunctions";
-import currentFutureGoalReducer from '../../reducers/goalsReducer/currentFutureGoalPageReducer'
+import {
+  getGlobalState,
+  dispatchInputChange
+} from "../../utils/helperFunctions";
+import currentFutureGoalReducer from "../../reducers/goalsReducer/currentFutureGoalPageReducer";
 
-import initialState from '../../initialState/currentFutureGoalsInitialState'
-import getCurrentGoals from '../../actions/currentFutureGoalsActions/getCurrentGoals'
-import getFutureGoals from '../../actions/currentFutureGoalsActions/getFutureGoals'
+import initialState from "../../initialState/currentFutureGoalsInitialState";
+import getCurrentGoals from "../../actions/currentFutureGoalsActions/getCurrentGoals";
+import getFutureGoals from "../../actions/currentFutureGoalsActions/getFutureGoals";
 
-import GoalNavBar from '../../components/navBars/goalNavBar'
+import GoalNavBar from "../../components/navBars/goalNavBar";
 import { Table } from "react-bootstrap";
-import '../../components/tables/DailyPointsTable.css'
-import CurrentGoalsHead from '../../components/tables/heads/goalsHead'
-import CurrentGoalRow from '../../components/tables/rows/currentGoalRow'
-import {withRouter} from 'react-router-dom'
+import "../../components/tables/DailyPointsTable.css";
+import CurrentGoalsHead from "../../components/tables/heads/goalsHead";
+import CurrentGoalRow from "../../components/tables/rows/currentGoalRow";
+import { withRouter } from "react-router-dom";
 
-function CurrentFutureGoals(props){
-    const globalState = getGlobalState(useContext(store));
+// custom hooks
+import useGlobalState from "../../customHooks/customAuthHooks/useGlobalState";
 
-  const [state, dispatch] = useReducer(
-    currentFutureGoalReducer,
-    initialState
-  );
+function CurrentFutureGoals(props) {
+  const { globalState } = useGlobalState();
+
+  const [state, dispatch] = useReducer(currentFutureGoalReducer, initialState);
   const {
     isLoadingFutureGoals,
     isErrorFutureGoals,
@@ -35,43 +38,39 @@ function CurrentFutureGoals(props){
    */
   useEffect(() => {
     getFutureGoals(dispatch, globalState.token);
-    getCurrentGoals(dispatch, globalState.token)
+    getCurrentGoals(dispatch, globalState.token);
   }, []);
 
-    const createGoalRow = (goals) => {
-       const goalRow = goals.map(goal => {
-            return CurrentGoalRow(dispatch,globalState.token,goal,props.history )
-        })
-        return goalRow
-    }
+  const createGoalRow = goals => {
+    const goalRow = goals.map(goal => {
+      return CurrentGoalRow(dispatch, globalState.token, goal, props.history);
+    });
+    return goalRow;
+  };
 
-    return (
-        <div className='rulePageContainer'>
-            <GoalNavBar />
-            <h2>Current Goals</h2>
-            <div>
-                {currentGoals?
-                <Table className="tableContainer">
-                    <CurrentGoalsHead />
-                    <tbody>
-                        {createGoalRow(currentGoals)}
-                    </tbody>
-                </Table> : null}
-            </div>
-            <div>
-                <h2>
-                    Future Goals
-                </h2>
-                {futureGoals?
-                <Table className="tableContainer">
-                    <CurrentGoalsHead />
-                    <tbody>
-                        {createGoalRow(futureGoals)}
-                    </tbody>
-                </Table> : null}
-            </div>
-        </div>
-    )
+  return (
+    <div className="rulePageContainer">
+      <GoalNavBar />
+      <h2>Current Goals</h2>
+      <div>
+        {currentGoals ? (
+          <Table className="tableContainer">
+            <CurrentGoalsHead />
+            <tbody>{createGoalRow(currentGoals)}</tbody>
+          </Table>
+        ) : null}
+      </div>
+      <div>
+        <h2>Future Goals</h2>
+        {futureGoals ? (
+          <Table className="tableContainer">
+            <CurrentGoalsHead />
+            <tbody>{createGoalRow(futureGoals)}</tbody>
+          </Table>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 export default withRouter(CurrentFutureGoals);
