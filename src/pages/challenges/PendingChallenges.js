11391a7088e6ef4bb88f20b5b5d056b63757ae4f
Joseph Wyman <jwymandev@gmail.com>
2020-04-18 11:46:38 -0400

    Add custom global hook to pending challenges page

--- src/pages/challenges/PendingChallenges.js
+++ src/pages/challenges/PendingChallenges.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useReducer, useContext } from "react";
-import { getGlobalState, dispatchInputChange } from "../../utils/helperFunctions";
+import {
+  getGlobalState,
+  dispatchInputChange
+} from "../../utils/helperFunctions";
 import pendingChallengePageReducer from "../../reducers/challengeReducers/pendingChallengesPageReducer";
 import initialState from "../../initialState/pendingChallengesInitialState";
 import getAllPendingChallengeInvitations from "../../actions/pendingChallengeInvitationActions/getAllPendingChallenges";
 import { store } from "../../store/globalStore";
 import createPendingChallengeRows from "../../components/tables/rows/pendingChallengeRow";
-import ChallengeNavBar from '../../components/navBars/challengeNavBar'
+import ChallengeNavBar from "../../components/navBars/challengeNavBar";
 import { Table } from "react-bootstrap";
-import '../../components/tables/DailyPointsTable.css'
-import PendingInvitationTableHead from '../../components/tables/heads/pendingInvitationsTableHead'
-
+import "../../components/tables/DailyPointsTable.css";
+import PendingInvitationTableHead from "../../components/tables/heads/pendingInvitationsTableHead";
+// custom hooks
+import useGlobalState from "../../customHooks/customAuthHooks/useGlobalState";
 
 /**
  * Display user's pending challenges allowing user to accept or reject them.
  * @param {*} props
  */
 function PendingChallengesPage(props) {
-  const globalState = getGlobalState(useContext(store));
+  const { globalState } = useGlobalState();
   const [state, dispatch] = useReducer(
     pendingChallengePageReducer,
     initialState
@@ -34,32 +38,42 @@ function PendingChallengesPage(props) {
   }, []);
 
   return (
-    <div className='rulePageContainer'>
+    <div className="rulePageContainer">
       <ChallengeNavBar />
-     <h1>Pending Challenges</h1>
+      <h1>Pending Challenges</h1>
 
       <div>
         {pendingChallenges.length > 0 ? (
           <Table className="tableContainer">
-         <PendingInvitationTableHead />
-          <tbody>
-            {pendingChallenges.map(challengeInvitation =>
-              createPendingChallengeRows(
-                dispatch,
-                globalState.token,
-                challengeInvitation
-              )
-            )}
-          </tbody>
-        </Table>
-         
-        ) : <div data-testid='noPendingChallengesMsg'> You have no pending challenges </div>}
+            <PendingInvitationTableHead />
+            <tbody>
+              {pendingChallenges.map(challengeInvitation =>
+                createPendingChallengeRows(
+                  dispatch,
+                  globalState.token,
+                  challengeInvitation
+                )
+              )}
+            </tbody>
+          </Table>
+        ) : (
+          <div data-testid="noPendingChallengesMsg">
+            {" "}
+            You have no pending challenges{" "}
+          </div>
+        )}
       </div>
       {isLoading ? <div>loading invitation challenges </div> : null}
-      {updateisLoading ? <div data-testid='updateisLoading'>updating status</div> : null}
-      {isError ? <div data-testid='isError'>error loading invitation challenges</div> : null}
+      {updateisLoading ? (
+        <div data-testid="updateisLoading">updating status</div>
+      ) : null}
+      {isError ? (
+        <div data-testid="isError">error loading invitation challenges</div>
+      ) : null}
       {updateisError ? (
-        <div data-testid='updateisError'>error updating invitation challenge status</div>
+        <div data-testid="updateisError">
+          error updating invitation challenge status
+        </div>
       ) : null}
     </div>
   );
