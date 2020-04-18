14745a3ab7c9c79f87fe3e08ed8e8b2960684622
Joseph Wyman <jwymandev@gmail.com>
2020-04-18 11:52:54 -0400

    add global custom hook to PastGoals

--- src/pages/goals/PastGoals.js
+++ src/pages/goals/PastGoals.js
@@ -1,32 +1,30 @@
 import React, { useEffect, useReducer, useContext } from "react";
 import { store } from "../../store/globalStore";
-import { getGlobalState, dispatchInputChange } from "../../utils/helperFunctions";
-import pastGoalsReducer from '../../reducers/goalsReducer/pastGoalsReducer'
+import {
+  getGlobalState,
+  dispatchInputChange
+} from "../../utils/helperFunctions";
+import pastGoalsReducer from "../../reducers/goalsReducer/pastGoalsReducer";
 
-import initialState from '../../initialState/pastGoalsState'
-import getPastGoals from '../../actions/pastGoalsPAge/getPastGoals'
+import initialState from "../../initialState/pastGoalsState";
+import getPastGoals from "../../actions/pastGoalsPAge/getPastGoals";
 
-import GoalNavBar from '../../components/navBars/goalNavBar'
+import GoalNavBar from "../../components/navBars/goalNavBar";
 import { Table } from "react-bootstrap";
-import '../../components/tables/DailyPointsTable.css'
-import PastGoalsHead from '../../components/tables/heads/pastGoalsHead'
-import PastGoalsRows from '../../components/tables/rows/pastGoalsRows'
-import GoalRows from '../../components/tables/rows/currentGoalRow'
-import {withRouter} from 'react-router-dom'
+import "../../components/tables/DailyPointsTable.css";
+import PastGoalsHead from "../../components/tables/heads/pastGoalsHead";
+import PastGoalsRows from "../../components/tables/rows/pastGoalsRows";
+import GoalRows from "../../components/tables/rows/currentGoalRow";
+import { withRouter } from "react-router-dom";
+// custom hooks
+import useGlobalState from "../../customHooks/customAuthHooks/useGlobalState";
 
-function PastGoalsPage(props){
-    const globalState = getGlobalState(useContext(store));
+function PastGoalsPage(props) {
+  const { globalState } = useGlobalState();
 
-  const [state, dispatch] = useReducer(
-    pastGoalsReducer,
-    initialState
-  );
+  const [state, dispatch] = useReducer(pastGoalsReducer, initialState);
 
-  const {
-    isLoadingPastGoals,
-    isErrorPastGoals,
-    pastGoals,
-  } = state;
+  const { isLoadingPastGoals, isErrorPastGoals, pastGoals } = state;
 
   /**
    * Fetch current and future goals.
@@ -35,32 +33,28 @@ function PastGoalsPage(props){
     getPastGoals(dispatch, globalState.token);
   }, []);
 
+  const createGoalRow = goals => {
+    const goalRow = goals.map(goal => {
+      //return PastGoalsRows(goal)
+      return GoalRows(dispatch, globalState.token, goal, props.history, true);
+    });
+    return goalRow;
+  };
 
-    const createGoalRow = (goals) => {
-       const goalRow = goals.map(goal => {
-            //return PastGoalsRows(goal)
-            return GoalRows(dispatch,globalState.token,goal, props.history, true)
-        })
-        return goalRow
-    }
-
-    return (
-        <div className='rulePageContainer'>
-            <GoalNavBar />
-            <h2>Past Goals</h2>
-            <div>
-                {pastGoals?
- 
-                <Table className="tableContainer">
-                    
-                    <PastGoalsHead />
-                    <tbody>
-                        {createGoalRow(pastGoals)}
-                    </tbody>
-                </Table> : null}
-            </div>
-        </div>
-    )
+  return (
+    <div className="rulePageContainer">
+      <GoalNavBar />
+      <h2>Past Goals</h2>
+      <div>
+        {pastGoals ? (
+          <Table className="tableContainer">
+            <PastGoalsHead />
+            <tbody>{createGoalRow(pastGoals)}</tbody>
+          </Table>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 export default withRouter(PastGoalsPage);
