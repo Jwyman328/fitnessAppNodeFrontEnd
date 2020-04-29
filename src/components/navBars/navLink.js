6b9ded62c2965efd1e1cca838b9a3b8feb97a545
Joseph Wyman <jwymandev@gmail.com>
2020-04-29 11:01:23 -0400

    Finish documentation of nav bars

--- src/components/navBars/navLink.js
+++ src/components/navBars/navLink.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import {withRouter} from 'react-router-dom'
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-function NavLink(props){
-    const handleClick = () => {
-        props.history.push(props.destination)
-    }
+/**
+ * Navigation link element to route to a specific page.
+ *
+ * @param {Function} props.history  router to navigate to other site pages.
+ * @param {String} props.linkName   text of where the route is taking the user.
+ * @param {String} props.testid     testid for testing.
+ */
+function NavLink(props) {
+  const handleClick = () => {
+    props.history.push(props.destination);
+  };
 
-    return (
-        <div data-testid={props.testid} className='navLink' onClick={handleClick}>
-            {props.linkName}
-        </div>
-    )
+  return (
+    <div data-testid={props.testid} className="navLink" onClick={handleClick}>
+      {props.linkName}
+    </div>
+  );
 }
 
 export default withRouter(NavLink);
--- src/components/navBars/resultsNavBar.js
+++ src/components/navBars/resultsNavBar.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./authNavBar.scss";
 import NavLink from "../navBars/navLink";
 
+/**
+ * Nav bar for navigating to results pages.
+ */
 function ResultsNavBar() {
   return (
     <div className="subNavContainer">
