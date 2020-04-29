699388a436686a4dbdc5476b350662187fdaeed9
Joseph Wyman <jwymandev@gmail.com>
2020-04-29 09:47:18 -0400

    Document linkButton

--- src/components/buttons/linkButton.js
+++ src/components/buttons/linkButton.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./linkButton.scss";
 import { withRouter } from "react-router-dom";
 
+/**
+ * Display a button that can route to a given destination.
+ *
+ * @param {String}   props.name     button name text.
+ * @param {Function} props.history  router to navigate to other site pages.
+ */
 function LinkButton(props) {
+  /**
+   * Navigate to a destination url.
+   */
   const navigateTo = () => {
     props.history.push(props.destination);
   };
